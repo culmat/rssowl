166bd082672fed120c92a4e449452812cafee703
bpasero <bpasero@f6fc0b4b-8082-4f21-9ead-586da66f3ff6>
2007-04-09 21:05:20 +0000

    Added TODO Comment

--- trunk/org.rssowl.core/src/org/rssowl/core/model/persist/NewsCounterItem.java
+++ trunk/org.rssowl.core/src/org/rssowl/core/model/persist/NewsCounterItem.java
@@ -21,54 +21,61 @@
  **     RSSOwl Development Team - initial API and implementation             **
  **                                                                          **
  **  **********************************************************************  */
+
 package org.rssowl.core.model.persist;
 
 import org.eclipse.core.runtime.Assert;
 import org.rssowl.core.model.internal.persist.Persistable;
 
-public final class NewsCounterItem extends Persistable implements IPersistable  {
+/**
+ * <p>
+ * TODO Consider contributing the NewsCounter from org.rssowl.ui if its only
+ * used from that bundle.
+ * </p>
+ */
+public final class NewsCounterItem extends Persistable implements IPersistable {
   private int newCounter;
   private int unreadCounter;
   private int stickyCounter;
-  
+
   public NewsCounterItem() {
-    
+
   }
-  
+
   public final int getNewCounter() {
     return newCounter;
   }
-  
+
   public final void incrementNewCounter() {
     ++newCounter;
   }
-  
+
   public final void decrementNewCounter() {
     Assert.isTrue(newCounter > 0, "newCounter must not be negative"); //$NON-NLS-1$
     --newCounter;
   }
-  
+
   public final int getUnreadCounter() {
     return unreadCounter;
   }
-  
+
   public final void incrementUnreadCounter() {
     ++unreadCounter;
   }
-  
+
   public final void decrementUnreadCounter() {
     Assert.isTrue(unreadCounter > 0, "unreadCounter must not be negative"); //$NON-NLS-1$
     --unreadCounter;
   }
-  
+
   public final int getStickyCounter() {
     return stickyCounter;
   }
-  
+
   public final void incrementStickyCounter() {
     ++stickyCounter;
   }
-  
+
   public final void decrementStickyCounter() {
     Assert.isTrue(stickyCounter > 0, "stickyCounter must not be negative"); //$NON-NLS-1$
     --stickyCounter;
