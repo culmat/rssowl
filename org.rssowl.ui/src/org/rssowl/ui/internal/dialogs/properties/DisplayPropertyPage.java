251ab304d253eee25ddde5f14e8e4a7cdd10bfbe
ijuma <ijuma@f6fc0b4b-8082-4f21-9ead-586da66f3ff6>
2007-04-09 20:01:04 +0000

    ASSIGNED - bug 366: Make persisted types more distinct (Entities, Types, SimpleTypes) http://dev.rssowl.org/show_bug.cgi?id=366#c6

--- org.rssowl.ui/src/org/rssowl/ui/internal/dialogs/properties/DisplayPropertyPage.java
+++ org.rssowl.ui/src/org/rssowl/ui/internal/dialogs/properties/DisplayPropertyPage.java
@@ -37,7 +37,7 @@ import org.rssowl.core.model.NewsModel;
 import org.rssowl.core.model.persist.IEntity;
 import org.rssowl.core.model.persist.IFolder;
 import org.rssowl.core.model.persist.IMark;
-import org.rssowl.core.model.preferences.IPreferencesScope;
+import org.rssowl.core.model.persist.pref.IPreferencesScope;
 import org.rssowl.ui.dialogs.properties.IEntityPropertyPage;
 import org.rssowl.ui.dialogs.properties.IPropertyDialogSite;
 import org.rssowl.ui.internal.editors.feed.NewsFilter;
