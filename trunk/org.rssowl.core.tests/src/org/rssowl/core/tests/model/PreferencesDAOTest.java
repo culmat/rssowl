b5bb723f8731dcf6319d145271dde19fab64bb3d
ijuma <ijuma@f6fc0b4b-8082-4f21-9ead-586da66f3ff6>
2007-04-14 18:23:35 +0000

    NEW - bug 369: Implement per Entity DAOs http://dev.rssowl.org/show_bug.cgi?id=369#c17

--- trunk/org.rssowl.core.tests/src/org/rssowl/core/tests/model/PreferencesDAOTest.java
+++ trunk/org.rssowl.core.tests/src/org/rssowl/core/tests/model/PreferencesDAOTest.java
@@ -33,7 +33,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.rssowl.core.Owl;
 import org.rssowl.core.model.dao.PersistenceException;
-import org.rssowl.core.model.persist.pref.IPreferencesDAO;
+import org.rssowl.core.model.persist.dao.IPreferencesDAO;
 import org.rssowl.core.model.persist.pref.PreferencesEvent;
 import org.rssowl.core.model.persist.pref.PreferencesListener;
 import org.rssowl.core.tests.TestUtils;
