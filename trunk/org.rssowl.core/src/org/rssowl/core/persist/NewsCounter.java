7b0daf22b312828dda924c557dd982ea0ee8198a
ijuma <ijuma@f6fc0b4b-8082-4f21-9ead-586da66f3ff6>
2007-04-30 01:20:50 +0000

    Make NewsCounter final because it's impossible to extend it without relying on internal API (Persistable).

--- trunk/org.rssowl.core/src/org/rssowl/core/persist/NewsCounter.java
+++ trunk/org.rssowl.core/src/org/rssowl/core/persist/NewsCounter.java
@@ -40,7 +40,7 @@ import java.util.Map;
  *
  * @see NewsCounterItem
  */
-public class NewsCounter extends Persistable {
+public final class NewsCounter extends Persistable {
   private Map<String, NewsCounterItem> fCountersMap;
 
   /** Leave default constructor for reflection. */
