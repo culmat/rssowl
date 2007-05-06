9f095353cd9c16f0b6403fdb31cf56172faaa072
bpasero <bpasero@f6fc0b4b-8082-4f21-9ead-586da66f3ff6>
2007-05-06 16:53:18 +0000

    Cache the sm icon for new news

--- org.rssowl.ui/src/org/rssowl/ui/internal/OwlUI.java
+++ org.rssowl.ui/src/org/rssowl/ui/internal/OwlUI.java
@@ -109,6 +109,9 @@ public class OwlUI {
   /** SearchMark */
   public static final ImageDescriptor SEARCHMARK = Activator.getImageDescriptor("icons/obj16/searchmark.gif"); //$NON-NLS-1$
 
+  /** SearchMark (New) */
+  public static final ImageDescriptor SEARCHMARK_NEW = Activator.getImageDescriptor("icons/obj16/searchmark_new.gif"); //$NON-NLS-1$
+
   /** Group */
   public static final ImageDescriptor GROUP = Activator.getImageDescriptor("icons/obj16/group.gif"); //$NON-NLS-1$
 
