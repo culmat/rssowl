800b1fe30bfcf4890a1315add38adccd7f748de9
bpasero <bpasero@f6fc0b4b-8082-4f21-9ead-586da66f3ff6>
2007-05-13 18:23:57 +0000

    Workaround for Bug 444

--- org.rssowl.ui/src/org/rssowl/ui/internal/editors/feed/NewsTableLabelProvider.java
+++ org.rssowl.ui/src/org/rssowl/ui/internal/editors/feed/NewsTableLabelProvider.java
@@ -49,6 +49,7 @@ import org.rssowl.core.persist.INews;
 import org.rssowl.core.persist.IPerson;
 import org.rssowl.core.util.DateUtils;
 import org.rssowl.core.util.StringUtils;
+import org.rssowl.ui.internal.Application;
 import org.rssowl.ui.internal.EntityGroup;
 import org.rssowl.ui.internal.OwlUI;
 import org.rssowl.ui.internal.util.ModelUtils;
@@ -347,8 +348,8 @@ public class NewsTableLabelProvider extends OwnerDrawLabelProvider {
     final Scrollable scrollable = (Scrollable) event.widget;
     GC gc = event.gc;
 
-    /* Handle selected News */
-    if ((event.detail & SWT.SELECTED) != 0) {
+    /* Handle selected News (Linux: Note Bug 444) */
+    if ((event.detail & SWT.SELECTED) != 0 && !Application.IS_LINUX) {
 
       /* Some conditions under which we don't override the selection color */
       if (news.getLabel() == null || !scrollable.isFocusControl())
