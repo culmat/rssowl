f8c744005d81c94aea28a1f3e0102eacdb02ac91
bpasero <bpasero@f6fc0b4b-8082-4f21-9ead-586da66f3ff6>
2007-08-02 21:37:30 +0000

    Bug 513: Progress reporting during reload

--- org.rssowl.ui/src/org/rssowl/ui/internal/Controller.java
+++ org.rssowl.ui/src/org/rssowl/ui/internal/Controller.java
@@ -218,7 +218,7 @@ public class Controller {
 
   private Controller() {
     fReloadFeedQueue = new JobQueue("Updating Feeds", MAX_CONCURRENT_RELOAD_JOBS, true, 0);
-    fSaveFeedQueue = new JobQueue("Saving Feeds", MAX_CONCURRENT_SAVE_JOBS, false, 0);
+    fSaveFeedQueue = new JobQueue("Updating Feeds", MAX_CONCURRENT_SAVE_JOBS, true, 0);
     fEntityPropertyPages = loadEntityPropertyPages();
     fBookMarkDAO = DynamicDAO.getDAO(IBookMarkDAO.class);
     fConditionalGetDAO = DynamicDAO.getDAO(IConditionalGetDAO.class);
