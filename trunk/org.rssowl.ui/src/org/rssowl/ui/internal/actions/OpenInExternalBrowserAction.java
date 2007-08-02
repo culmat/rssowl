190fb2af7d04ac8071960b75591e7daf44f1be6e
bpasero <bpasero@f6fc0b4b-8082-4f21-9ead-586da66f3ff6>
2007-08-02 21:17:19 +0000

    - Bug 496: Add a "show in external browser" action

---
+++ trunk/org.rssowl.ui/src/org/rssowl/ui/internal/actions/OpenInExternalBrowserAction.java
@@ -0,0 +1,74 @@
+/*   **********************************************************************  **
+ **   Copyright notice                                                       **
+ **                                                                          **
+ **   (c) 2005-2006 RSSOwl Development Team                                  **
+ **   http://www.rssowl.org/                                                 **
+ **                                                                          **
+ **   All rights reserved                                                    **
+ **                                                                          **
+ **   This program and the accompanying materials are made available under   **
+ **   the terms of the Eclipse Public License v1.0 which accompanies this    **
+ **   distribution, and is available at:                                     **
+ **   http://www.rssowl.org/legal/epl-v10.html                               **
+ **                                                                          **
+ **   A copy is found in the file epl-v10.html and important notices to the  **
+ **   license from the team is found in the textfile LICENSE.txt distributed **
+ **   in this package.                                                       **
+ **                                                                          **
+ **   This copyright notice MUST APPEAR in all copies of the file!           **
+ **                                                                          **
+ **   Contributors:                                                          **
+ **     RSSOwl Development Team - initial API and implementation             **
+ **                                                                          **
+ **  **********************************************************************  */
+
+package org.rssowl.ui.internal.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.rssowl.core.persist.INews;
+import org.rssowl.ui.internal.util.BrowserUtils;
+
+import java.util.List;
+
+/**
+ * @author bpasero
+ */
+public class OpenInExternalBrowserAction implements IWorkbenchWindowActionDelegate {
+  private IStructuredSelection fSelection;
+
+  /*
+   * @see org.eclipse.ui.IWorkbenchWindowActionDelegate#dispose()
+   */
+  public void dispose() {}
+
+  /*
+   * @see org.eclipse.ui.IWorkbenchWindowActionDelegate#init(org.eclipse.ui.IWorkbenchWindow)
+   */
+  public void init(IWorkbenchWindow window) {}
+
+  /*
+   * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
+   */
+  public void run(IAction action) {
+    List<?> selection = fSelection.toList();
+    for (Object object : selection) {
+      if (object instanceof INews) {
+        INews news = (INews) object;
+        if (news.getLink() != null)
+          BrowserUtils.openLink(news.getLink().toString());
+      }
+    }
+  }
+
+  /*
+   * @see org.eclipse.ui.IActionDelegate#selectionChanged(org.eclipse.jface.action.IAction, org.eclipse.jface.viewers.ISelection)
+   */
+  public void selectionChanged(IAction action, ISelection selection) {
+    if (selection instanceof IStructuredSelection)
+      fSelection = (IStructuredSelection) selection;
+  }
+}
