f8978beedf29e55fe7c0ca95a1eec45f3a26563d
bpasero <bpasero@f6fc0b4b-8082-4f21-9ead-586da66f3ff6>
2007-08-10 20:16:23 +0000

    Bug 454 - Allow to limit a Search on selected Folders and Marks

--- trunk/org.rssowl.core.tests/src/org/rssowl/core/tests/LocalTests.java
+++ trunk/org.rssowl.core.tests/src/org/rssowl/core/tests/LocalTests.java
@@ -43,7 +43,7 @@ import org.rssowl.core.tests.util.MergeUtilsTest;
 
 /**
  * Test-Suite for Core-Tests that are not requiring Network-Access.
- * 
+ *
  * @author bpasero
  * @author Ismael Juma (ismael@juma.me.uk)
  */
