62bd8b3ba5bb68f020c8856a4388164294f03d2e
bpasero <bpasero@f6fc0b4b-8082-4f21-9ead-586da66f3ff6>
2007-10-01 18:00:04 +0000

    Return copies

--- org.rssowl.ui/src/org/rssowl/ui/internal/CacheService.java
+++ org.rssowl.ui/src/org/rssowl/ui/internal/CacheService.java
@@ -170,24 +170,24 @@ public class CacheService {
   }
 
   /**
-   * Returns an unmodifiable Set of <code>IFolder</code>s, having no parent
-   * Folder set, thereby being Root-Folders.
+   * Returns a Set of <code>IFolder</code>s, having no parent Folder set,
+   * thereby being Root-Folders.
    *
-   * @return an unmodifiable Set of all Root-Folders.
+   * @return a Set of all Root-Folders.
    */
   public Set<IFolder> getRootFolders() {
-    return Collections.unmodifiableSet(fRootFolders);
+    return new HashSet<IFolder>(fRootFolders);
   }
 
   /**
-   * Returns an unmodifiable Set of all <code>IBookMark</code>s stored in the
-   * persistence layer.
+   * Returns a Set of all <code>IBookMark</code>s stored in the persistence
+   * layer.
    *
-   * @return an unmodifiable Set of all <code>IBookMark</code>s stored in the
-   * persistence layer.
+   * @return a Set of all <code>IBookMark</code>s stored in the persistence
+   * layer.
    */
   public Set<IBookMark> getBookMarks() {
-    return Collections.unmodifiableSet(fBookMarks);
+    return new HashSet<IBookMark>(fBookMarks);
   }
 
   /**
@@ -206,14 +206,14 @@ public class CacheService {
   }
 
   /**
-   * Returns an unmodifiable Set of all <code>ISearchMark</code>s stored in
-   * the persistence layer.
+   * Returns a Set of all <code>ISearchMark</code>s stored in the persistence
+   * layer.
    *
-   * @return an unmodifiable Set of all <code>ISearchMark</code>s stored in
-   * the persistence layer.
+   * @return a Set of all <code>ISearchMark</code>s stored in the persistence
+   * layer.
    */
   public Set<ISearchMark> getSearchMarks() {
-    return Collections.unmodifiableSet(fSearchMarks);
+    return new HashSet<ISearchMark>(fSearchMarks);
   }
 
   /**
