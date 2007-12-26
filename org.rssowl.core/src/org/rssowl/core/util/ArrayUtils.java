3feb9302578dcd9d135821183f6f787e55b3271f
ijuma <ijuma@f6fc0b4b-8082-4f21-9ead-586da66f3ff6>
2007-12-26 13:39:27 +0000

    Initial version.

---
+++ org.rssowl.core/src/org/rssowl/core/util/ArrayUtils.java
@@ -0,0 +1,69 @@
+/*   **********************************************************************  **
+ **   Copyright notice                                                       **
+ **                                                                          **
+ **   (c) 2005-2006 RSSOwl Development Team                                  **
+ **   http://www.rssowl.org/                                                 **
+ **                                                                          **
+ **   All rights reserved                                                    **
+ **                                                                          **
+ **   This program and the accompanying materials are made available under   **
+ **   the terms of the Eclipse Public License v1.0 which accompanies this    **
+ **   distribution, and is available at:                                     **
+ **   http://www.rssowl.org/legal/epl-v10.html                               **
+ **                                                                          **
+ **   A copy is found in the file epl-v10.html and important notices to the  **
+ **   license from the team is found in the textfile LICENSE.txt distributed **
+ **   in this package.                                                       **
+ **                                                                          **
+ **   This copyright notice MUST APPEAR in all copies of the file!           **
+ **                                                                          **
+ **   Contributors:                                                          **
+ **     RSSOwl Development Team - initial API and implementation             **
+ **                                                                          **
+ **  **********************************************************************  */
+package org.rssowl.core.util;
+
+public final class ArrayUtils {
+
+  private ArrayUtils() {
+    super();
+  }
+
+  public static void reverse(int[] newsToCleanUp, int newsToCleanUpSize) {
+    for (int left = 0, right = newsToCleanUpSize -1 ; left < right; left++, right--) {
+      int temp = newsToCleanUp[left];
+      newsToCleanUp[left]  = newsToCleanUp[right];
+      newsToCleanUp[right] = temp;
+    }
+  }
+
+  public static int[] ensureCapacity(int[] array, int minCapacity) {
+    int oldCapacity = array.length;
+    if (minCapacity > oldCapacity) {
+      int[] oldData = array;
+      int newCapacity = (oldCapacity * 3) / 2 + 1;
+      if (newCapacity < minCapacity)
+        newCapacity = minCapacity;
+      // minCapacity is usually close to size, so this is a win:
+      int[] copy = new int[newCapacity];
+      System.arraycopy(array, 0, copy, 0, Math.min(oldData.length, newCapacity));
+      return copy;
+    }
+    return array;
+  }
+
+  public static long[] ensureCapacity(long[] array, int minCapacity) {
+    int oldCapacity = array.length;
+    if (minCapacity > oldCapacity) {
+      long[] oldData = array;
+      int newCapacity = (oldCapacity * 3) / 2 + 1;
+      if (newCapacity < minCapacity)
+        newCapacity = minCapacity;
+      // minCapacity is usually close to size, so this is a win:
+      long[] copy = new long[newCapacity];
+      System.arraycopy(array, 0, copy, 0, Math.min(oldData.length, newCapacity));
+      return copy;
+    }
+    return array;
+  }
+}
