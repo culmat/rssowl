89d49c6d4c9e7ff9386d1ca21483e862d4505dcd
ijuma <ijuma@f6fc0b4b-8082-4f21-9ead-586da66f3ff6>
2007-12-28 22:18:47 +0000

    Delegate to NewsContainer most functionality.

--- trunk/org.rssowl.core/src/org/rssowl/core/internal/persist/NewsBin.java
+++ trunk/org.rssowl.core/src/org/rssowl/core/internal/persist/NewsBin.java
@@ -23,29 +23,18 @@
  **  **********************************************************************  */
 package org.rssowl.core.internal.persist;
 
-import org.eclipse.core.runtime.Assert;
 import org.rssowl.core.persist.IFolder;
 import org.rssowl.core.persist.INews;
 import org.rssowl.core.persist.INewsBin;
 import org.rssowl.core.persist.INews.State;
 import org.rssowl.core.persist.reference.NewsReference;
-import org.rssowl.core.util.ArrayUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-//TODO What happens to hidden and deleted news?
 public class NewsBin extends Mark implements INewsBin   {
 
-  private long[] fReadNews = new long[0];
-  private int fReadNewsSize = 0;
-
-  private long[] fUnreadUpdatedNews = new long[0];
-  private int fUnreadUpdatedNewsSize = 0;
-
-  private long[] fNewNews = new long[0];
-  private int fNewNewsSize = 0;
+  private NewsContainer newsContainer;
 
   /**
    * Creates a new Element of the type SearchMark. A SearchMark is only visually
@@ -68,91 +57,22 @@ public class NewsBin extends Mark implements INewsBin   {
   }
 
   public void addNews(INews news) {
-    switch (news.getState()) {
-      case NEW:
-        fNewNews = addNews(fNewNews, news, fNewNewsSize);
-        ++fNewNewsSize;
-        break;
-      case READ:
-        fReadNews = addNews(fReadNews, news, fReadNewsSize);
-        ++fReadNewsSize;
-        break;
-      case UNREAD:
-      case UPDATED:
-        fUnreadUpdatedNews = addNews(fUnreadUpdatedNews, news, fUnreadUpdatedNewsSize);
-        ++fUnreadUpdatedNewsSize;
-        break;
-      default:
-        throw new IllegalArgumentException("news cannot be added with state: " + news.getState());
-    }
-  }
-
-  private long[] addNews(long[] newsArray, INews news, int currentArraySize) {
-    long[] array = ArrayUtils.ensureCapacity(newsArray, currentArraySize + 1);
-    array[currentArraySize] = news.getId();
-    return array;
-  }
-
-  public void removeNews(INews news) {
-
-  }
-
-  public int getNewsCount(Set<INews.State> states) {
-    Assert.isNotNull(states, "states");
-    int count = 0;
-
-    /* Read News */
-    if (states.contains(INews.State.READ))
-      count += fReadNewsSize;
-
-    //FIXME This is copied from SearchMark and it seems buggy to me. Ask ben
-    //what the motivation was
-    /* Unread or Updated News */
-    if (states.contains(INews.State.UNREAD) || states.contains(INews.State.UPDATED))
-      count += fUnreadUpdatedNewsSize;
-
-    /* New News */
-    if (states.contains(INews.State.NEW))
-      count += fNewNewsSize;
-
-    return count;
+    newsContainer.addNews(news);
   }
 
   public boolean containsNews(INews news) {
-    if (containsNews(fNewNews, news, fNewNewsSize)) {
-      return true;
-    } if (containsNews(fUnreadUpdatedNews, news, fUnreadUpdatedNewsSize)) {
-      return true;
-    }
-    return containsNews(fReadNews, news, fReadNewsSize);
-  }
-
-  private static boolean containsNews(long[] newsArray, INews news, int endIndex) {
-    return ArrayUtils.binarySearch(newsArray, news.getId(), endIndex) >= 0;
+    return newsContainer.containsNews(news);
   }
 
   public List<NewsReference> getNews() {
-    Set<State> states = INews.State.getVisible();
-    List<NewsReference> newsRefs = new ArrayList<NewsReference>(getNewsCount(states));
+    return newsContainer.getNews();
+  }
 
-    /* Add Read News */
-    if (states.contains(INews.State.READ)) {
-      for (int i = 0; i < fReadNewsSize; ++i)
-        newsRefs.add(new NewsReference(fReadNews[i]));
-    }
+  public int getNewsCount(Set<State> states) {
+    return newsContainer.getNewsCount(states);
+  }
 
-    /* Add Unread News */
-    if (states.contains(INews.State.UNREAD) || states.contains(INews.State.UPDATED)) {
-      for (int i = 0; i < fUnreadUpdatedNewsSize; ++i)
-        newsRefs.add(new NewsReference(fUnreadUpdatedNews[i]));
-    }
-
-    /* Add New News */
-    if (states.contains(INews.State.NEW)) {
-      for (int i = 0; i < fNewNewsSize; ++i)
-        newsRefs.add(new NewsReference(fNewNews[i]));
-    }
-
-    return newsRefs;
+  public void removeNews(INews news) {
+    newsContainer.removeNews(news);
   }
 }
