8ed45a15ec01246dce793b7b544e37dbcda7eb44
bpasero <bpasero@f6fc0b4b-8082-4f21-9ead-586da66f3ff6>
2008-01-13 19:41:15 +0000

    Bug 274: Support for Key-Word Subscriptions Bug 434: Improve the "Add BookMark "dialog box

---
+++ org.rssowl.ui/src/org/rssowl/ui/internal/dialogs/wizards/CreateBookmarkWizard.java
@@ -0,0 +1,221 @@
+/*   **********************************************************************  **
+ **   Copyright notice                                                       **
+ **                                                                          **
+ **   (c) 2005-2006 RSSOwl Development Team                                  **
+ **   http://www.rssowl.org/                                                 **
+ **                                                                          **
+ **   All rights reserved                                                    **
+ **                                                                          **
+ **   This program and the accompanying materials are made available under   **
+ **   the terms of the Eclipse Public License v1.0 which accompanies this    **
+ **   distribution, and is available at:                                     **
+ **   http://www.rssowl.org/legal/epl-v10.html                               **
+ **                                                                          **
+ **   A copy is found in the file epl-v10.html and important notices to the  **
+ **   license from the team is found in the textfile LICENSE.txt distributed **
+ **   in this package.                                                       **
+ **                                                                          **
+ **   This copyright notice MUST APPEAR in all copies of the file!           **
+ **                                                                          **
+ **   Contributors:                                                          **
+ **     RSSOwl Development Team - initial API and implementation             **
+ **                                                                          **
+ **  **********************************************************************  */
+
+package org.rssowl.ui.internal.dialogs.wizards;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.wizard.IWizardPage;
+import org.eclipse.jface.wizard.Wizard;
+import org.rssowl.core.Owl;
+import org.rssowl.core.connection.AuthenticationRequiredException;
+import org.rssowl.core.connection.ConnectionException;
+import org.rssowl.core.persist.IBookMark;
+import org.rssowl.core.persist.IFeed;
+import org.rssowl.core.persist.IFolder;
+import org.rssowl.core.persist.IMark;
+import org.rssowl.core.persist.dao.DynamicDAO;
+import org.rssowl.core.persist.dao.IFeedDAO;
+import org.rssowl.core.persist.reference.FeedLinkReference;
+import org.rssowl.core.persist.reference.FeedReference;
+import org.rssowl.core.util.StringUtils;
+import org.rssowl.ui.internal.Activator;
+import org.rssowl.ui.internal.actions.ReloadTypesAction;
+
+import java.lang.reflect.InvocationTargetException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Map;
+
+/**
+ * A {@link Wizard} to easily create a new {@link IBookMark}.
+ *
+ * @author bpasero
+ */
+public class CreateBookmarkWizard extends Wizard {
+  private FeedDefinitionPage fFeedDefinitionPage;
+  private KeywordSubscriptionPage fKeywordPage;
+  private BookmarkDefinitionPage fBookMarkDefinitionPage;
+  private final IFolder fSelection;
+  private final String fInitialLink;
+  private final IMark fPosition;
+
+  /**
+   * @param parent
+   * @param position
+   * @param initialLink
+   */
+  public CreateBookmarkWizard(IFolder parent, IMark position, String initialLink) {
+    fSelection = parent;
+    fPosition = position;
+    fInitialLink = initialLink;
+  }
+
+  /*
+   * @see org.eclipse.jface.wizard.Wizard#addPages()
+   */
+  @Override
+  public void addPages() {
+    setHelpAvailable(false);
+    setWindowTitle("New Bookmark");
+
+    /* Page 1: Enter Link or Keyword */
+    fFeedDefinitionPage = new FeedDefinitionPage("Bookmark", fInitialLink);
+    addPage(fFeedDefinitionPage);
+
+    /* Page 2: Choose Provider for Keyword Subscription (optional) */
+    fKeywordPage = new KeywordSubscriptionPage("Bookmark");
+    addPage(fKeywordPage);
+
+    /* Page 2: Define Name and Location */
+    fBookMarkDefinitionPage = new BookmarkDefinitionPage("Bookmark", fSelection);
+    addPage(fBookMarkDefinitionPage);
+  }
+
+  /*
+   * @see org.eclipse.jface.wizard.Wizard#getNextPage(org.eclipse.jface.wizard.IWizardPage)
+   */
+  @Override
+  public IWizardPage getNextPage(IWizardPage page) {
+
+    /* Special Treatment for Feed Definition Page */
+    if (page == fFeedDefinitionPage) {
+
+      /* Keyword Subscription */
+      if (fFeedDefinitionPage.isKeywordSubscription())
+        return fKeywordPage;
+
+      return fBookMarkDefinitionPage;
+    }
+
+    return super.getNextPage(page);
+  }
+
+  void loadNameForFeed() {
+
+    /* Keyword Subscription - Build from Search Engine */
+    if (fFeedDefinitionPage.isKeywordSubscription()) {
+      fBookMarkDefinitionPage.presetBookmarkName(fKeywordPage.getSelectedEngine().getLabel(fFeedDefinitionPage.getKeyword()));
+    }
+
+    /* Link Subscription - Load from Feed if requested */
+    else if (fFeedDefinitionPage.loadTitleFromFeed()) {
+      final String linkText = fFeedDefinitionPage.getLink();
+      IRunnableWithProgress runnable = new IRunnableWithProgress() {
+        public void run(IProgressMonitor monitor) {
+          monitor.beginTask("Loading title from Feed...", IProgressMonitor.UNKNOWN);
+
+          try {
+            final URI link = new URI(linkText);
+            String feedTitle = Owl.getConnectionService().getLabel(link);
+            if (StringUtils.isSet(feedTitle))
+              fBookMarkDefinitionPage.presetBookmarkName(feedTitle);
+          } catch (final ConnectionException e) {
+
+            /* Authentication Required */
+            if (e instanceof AuthenticationRequiredException) {
+              //TODO Rather show another Page for credentials then!
+            }
+          } catch (URISyntaxException e) {
+            Activator.getDefault().logError(e.getMessage(), e);
+          }
+        }
+      };
+
+      /* Perform Runnable in separate Thread and show progress */
+      try {
+        getContainer().run(true, false, runnable);
+      } catch (InvocationTargetException e) {
+        Activator.getDefault().logError(e.getMessage(), e);
+      } catch (InterruptedException e) {
+        Activator.getDefault().logError(e.getMessage(), e);
+      }
+    }
+  }
+
+  /*
+   * @see org.eclipse.jface.wizard.Wizard#performFinish()
+   */
+  @Override
+  public boolean performFinish() {
+    try {
+      internalPerformFinish();
+    } catch (URISyntaxException e) {
+      Activator.getDefault().logError(e.getMessage(), e);
+    }
+
+    return true;
+  }
+
+  private void internalPerformFinish() throws URISyntaxException {
+    URI uriObj;
+    if (fFeedDefinitionPage.isKeywordSubscription())
+      uriObj = new URI(fKeywordPage.getSelectedEngine().toUrl(fFeedDefinitionPage.getKeyword()));
+    else
+      uriObj = new URI(fFeedDefinitionPage.getLink());
+
+    IFeedDAO feedDAO = DynamicDAO.getDAO(IFeedDAO.class);
+
+    /* Check if a Feed with the URL already exists */
+    FeedReference feedRef = feedDAO.loadReference(uriObj);
+
+    /* Create a new Feed then */
+    if (feedRef == null) {
+      IFeed feed = Owl.getModelFactory().createFeed(null, uriObj);
+      feed = feedDAO.save(feed);
+    }
+
+    /* Create the BookMark */
+    IFolder parent = fBookMarkDefinitionPage.getFolder();
+    String title = fBookMarkDefinitionPage.getBookmarkName();
+
+    FeedLinkReference feedLinkRef = new FeedLinkReference(uriObj);
+    IBookMark bookmark = Owl.getModelFactory().createBookMark(null, parent, feedLinkRef, title, fPosition, fPosition != null ? true : null);
+
+    /* Copy all Properties from Parent into this Mark */
+    Map<String, ?> properties = parent.getProperties();
+
+    for (Map.Entry<String, ?> property : properties.entrySet())
+      bookmark.setProperty(property.getKey(), property.getValue());
+
+    parent = DynamicDAO.save(parent);
+
+    /* Auto-Reload added BookMark */
+    for (IMark mark : parent.getMarks()) {
+      if (mark.equals(bookmark)) {
+        new ReloadTypesAction(new StructuredSelection(mark), getShell()).run();
+        break;
+      }
+    }
+  }
+
+  /*
+   * @see org.eclipse.jface.wizard.Wizard#needsProgressMonitor()
+   */
+  @Override
+  public boolean needsProgressMonitor() {
+    return true;
+  }
+}
---
+++ org.rssowl.ui/src/org/rssowl/ui/internal/dialogs/wizards/KeywordSubscriptionPage.java
@@ -0,0 +1,157 @@
+/*   **********************************************************************  **
+ **   Copyright notice                                                       **
+ **                                                                          **
+ **   (c) 2005-2006 RSSOwl Development Team                                  **
+ **   http://www.rssowl.org/                                                 **
+ **                                                                          **
+ **   All rights reserved                                                    **
+ **                                                                          **
+ **   This program and the accompanying materials are made available under   **
+ **   the terms of the Eclipse Public License v1.0 which accompanies this    **
+ **   distribution, and is available at:                                     **
+ **   http://www.rssowl.org/legal/epl-v10.html                               **
+ **                                                                          **
+ **   A copy is found in the file epl-v10.html and important notices to the  **
+ **   license from the team is found in the textfile LICENSE.txt distributed **
+ **   in this package.                                                       **
+ **                                                                          **
+ **   This copyright notice MUST APPEAR in all copies of the file!           **
+ **                                                                          **
+ **   Contributors:                                                          **
+ **     RSSOwl Development Team - initial API and implementation             **
+ **                                                                          **
+ **  **********************************************************************  */
+
+package org.rssowl.ui.internal.dialogs.wizards;
+
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.rssowl.core.util.StringUtils;
+import org.rssowl.core.util.URIUtils;
+import org.rssowl.ui.internal.OwlUI;
+
+/**
+ * @author bpasero
+ */
+public class KeywordSubscriptionPage extends WizardPage {
+  private static final String URL_INPUT_TOKEN = " ";
+
+  /* Base class for Search Engines */
+  static class SearchEngine {
+    private final String fName;
+    private final String fIconPath;
+    private final String fUrl;
+
+    SearchEngine(String name, String iconPath, String url) {
+      fName = name;
+      fIconPath = iconPath;
+      fUrl = url;
+    }
+
+    String getName() {
+      return fName;
+    }
+
+    String getLabel(String keywords) {
+      return fName + " on '" + keywords + "'";
+    }
+
+    String getIconPath() {
+      return fIconPath;
+    }
+
+    String toUrl(String keywords) {
+      keywords = URIUtils.urlEncode(keywords);
+      return StringUtils.replaceAll(fUrl, URL_INPUT_TOKEN, keywords);
+    }
+  }
+
+  /* Supported Engines */
+  private static final SearchEngine[] fgSearchEngines = new SearchEngine[] {
+
+  /* Delicious */
+  new SearchEngine("Delicious", "icons/obj16/fav_delicious.gif", "http://del.icio.us/rss/tag/" + URL_INPUT_TOKEN),
+
+  /* Flickr Photo */
+  new SearchEngine("Flickr Photo", "icons/obj16/fav_flickr.gif", "http://www.flickr.com/services/feeds/photos_public.gne?tags=" + URL_INPUT_TOKEN + "&format=rss_200"),
+
+  /* Google Blog */
+  new SearchEngine("Google Blog", "icons/obj16/fav_google.gif", "http://blogsearch.google.com/blogsearch_feeds?q=" + URL_INPUT_TOKEN + "&num=10&output=rss"),
+
+  /* Google News */
+  new SearchEngine("Google News", "icons/obj16/fav_google.gif", "http://news.google.com/news?q=" + URL_INPUT_TOKEN + "&output=rss"),
+
+  /* Technorati */
+  new SearchEngine("Technorati", "icons/obj16/fav_technorati.gif", "http://feeds.technorati.com/search/" + URL_INPUT_TOKEN),
+
+  /* YouTube Video */
+  new SearchEngine("YouTube Video", "icons/obj16/fav_youtube.gif", "http://www.youtube.com/rss/tag/" + URL_INPUT_TOKEN + ".rss")
+
+  };
+
+  private SearchEngine fSelectedEngine;
+
+  /**
+   * @return Returns the currently selected SearchEngine.
+   */
+  public SearchEngine getSelectedEngine() {
+    return fSelectedEngine;
+  }
+
+  /**
+   * @param pageName
+   */
+  protected KeywordSubscriptionPage(String pageName) {
+    super(pageName, pageName, OwlUI.getImageDescriptor("icons/wizban/bkmrk_wiz.gif"));
+    setMessage("Create a new Bookmark to read News from a Feed.");
+  }
+
+  /*
+   * @see org.eclipse.jface.dialogs.IDialogPage#createControl(org.eclipse.swt.widgets.Composite)
+   */
+  /*
+   * @see org.eclipse.jface.dialogs.IDialogPage#createControl(org.eclipse.swt.widgets.Composite)
+   */
+  public void createControl(Composite parent) {
+    Composite container = new Composite(parent, SWT.NONE);
+    container.setLayout(new GridLayout(1, false));
+
+    Label infoLabel = new Label(container, SWT.NONE);
+    infoLabel.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false));
+    infoLabel.setText("Please select from one of the following search engines: ");
+
+    Composite contentMargin = new Composite(container, SWT.NONE);
+    contentMargin.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false));
+    contentMargin.setLayout(new GridLayout(1, false));
+    ((GridLayout) contentMargin.getLayout()).marginTop = 10;
+    ((GridLayout) contentMargin.getLayout()).verticalSpacing = 10;
+
+    for (int i = 0; i < fgSearchEngines.length; i++) {
+      final SearchEngine engine = fgSearchEngines[i];
+
+      Button button = new Button(contentMargin, SWT.RADIO);
+      button.setText(engine.getName());
+      button.setImage(OwlUI.getImage(button, engine.getIconPath()));
+      button.addSelectionListener(new SelectionAdapter() {
+        @Override
+        public void widgetSelected(SelectionEvent e) {
+          fSelectedEngine = engine;
+        }
+      });
+
+      if (i == 0) {
+        button.setSelection(true);
+        fSelectedEngine = engine;
+      }
+    }
+
+    setControl(container);
+  }
+}
