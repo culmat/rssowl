2ce74f9ecb17f06e9310097092d9366ffae96cbd
bpasero <bpasero@f6fc0b4b-8082-4f21-9ead-586da66f3ff6>
2008-02-09 20:15:50 +0000

    Rename package

--- trunk/org.rssowl.ui/src/org/rssowl/ui/internal/dialogs/bookmark/KeywordSubscriptionPage.java
+++ trunk/org.rssowl.ui/src/org/rssowl/ui/internal/dialogs/bookmark/KeywordSubscriptionPage.java
@@ -22,7 +22,7 @@
  **                                                                          **
  **  **********************************************************************  */
 
-package org.rssowl.ui.internal.dialogs.wizards;
+package org.rssowl.ui.internal.dialogs.bookmark;
 
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtensionRegistry;
