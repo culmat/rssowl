4718f63ab312eb769102d7676b84513c2eea7dd8
bpasero <bpasero@f6fc0b4b-8082-4f21-9ead-586da66f3ff6>
2008-09-27 16:40:13 +0000

    Bug 914: Opening links directly when title equals summary

--- org.rssowl.ui/src/org/rssowl/ui/internal/editors/feed/NewsBrowserControl.java
+++ org.rssowl.ui/src/org/rssowl/ui/internal/editors/feed/NewsBrowserControl.java
@@ -152,10 +152,20 @@ public class NewsBrowserControl implements IFeedViewPart {
       return news.getLinkAsText();
 
     boolean openEmptyNews = Owl.getPreferenceService().getGlobalScope().getBoolean(DefaultPreferences.BM_OPEN_SITE_FOR_EMPTY_NEWS);
-    if (openEmptyNews && !StringUtils.isSet(news.getDescription()))
+    if (openEmptyNews && isEmpty(news))
       return news.getLinkAsText();
 
     return news;
+  }
+
+  private boolean isEmpty(INews news) {
+    if (!StringUtils.isSet(news.getDescription()))
+      return true;
+
+    if (StringUtils.isSet(news.getTitle()) && news.getTitle().equals(news.getDescription()))
+      return true;
+
+    return false;
   }
 
   /*
