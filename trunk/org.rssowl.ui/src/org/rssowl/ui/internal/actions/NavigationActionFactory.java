3a66bedecd678049390ebddc44ccec118b85c41e
bpasero <bpasero@f6fc0b4b-8082-4f21-9ead-586da66f3ff6>
2009-05-26 16:07:40 +0000

    Bug 1063 Add Next/Previous Tab to the Go menu

--- trunk/org.rssowl.ui/src/org/rssowl/ui/internal/actions/NavigationActionFactory.java
+++ trunk/org.rssowl.ui/src/org/rssowl/ui/internal/actions/NavigationActionFactory.java
@@ -30,13 +30,18 @@ import org.eclipse.core.runtime.IExecutableExtension;
 import org.eclipse.core.runtime.IExecutableExtensionFactory;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IEditorReference;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.PartInitException;
 import org.rssowl.ui.internal.Activator;
 import org.rssowl.ui.internal.OwlUI;
 import org.rssowl.ui.internal.editors.feed.FeedView;
 import org.rssowl.ui.internal.views.explorer.BookMarkExplorer;
+
+import java.util.List;
 
 /**
  * The <code>NavigationActionFactory</code> is providing a list of common
@@ -61,17 +66,68 @@ public class NavigationActionFactory implements IExecutableExtensionFactory, IEx
 
     public void run(IAction action) {
 
-      /* 1.) Navigate in opened FeedViews */
-      if (navigateOnActiveFeedView())
+      /* Tab Navigation */
+      if (fType == Actions.NEXT_TAB || fType == Actions.PREVIOUS_TAB) {
+        navigateInTabs();
+      }
+
+      /* News/Feed Navigation */
+      else {
+
+        /* 1.) Navigate in opened FeedViews */
+        if (navigateOnActiveFeedView())
+          return;
+
+        /* 2.) Navigate in opened Explorer */
+        if (navigateOnOpenExplorer())
+          return;
+
+        /* 3.) Navigate on entire Model */
+        if (navigateOnModel())
+          return;
+      }
+    }
+
+    private void navigateInTabs() {
+
+      /* Current Active Editor */
+      IEditorPart activeEditor = OwlUI.getActiveEditor();
+      if (activeEditor == null)
         return;
 
-      /* 2.) Navigate in opened Explorer */
-      if (navigateOnOpenExplorer())
+      List<IEditorReference> editors = OwlUI.getEditorReferences();
+
+      int index = -1;
+      for (int i = 0; i < editors.size(); i++) {
+        try {
+          if (activeEditor.getEditorInput().equals(editors.get(i).getEditorInput())) {
+            index = i;
+            break;
+          }
+        } catch (PartInitException e) {
+          Activator.getDefault().logError(e.getMessage(), e);
+        }
+      }
+
+      if (index < 0)
         return;
 
-      /* 3.) Navigate on entire Model */
-      if (navigateOnModel())
-        return;
+      IEditorPart tab = null;
+
+      /* Next Tab */
+      if (fType == Actions.NEXT_TAB)
+        tab = editors.get(index + 1 < editors.size() ? index + 1 : 0).getEditor(true);
+
+      /* Previous Tab */
+      else if (fType == Actions.PREVIOUS_TAB)
+        tab = editors.get(index - 1 >= 0 ? index - 1 : editors.size() - 1).getEditor(true);
+
+      /* Activate */
+      if (tab != null) {
+        IWorkbenchPage page = tab.getSite().getPage();
+        page.activate(tab.getSite().getPart());
+        page.activate(tab);
+      }
     }
 
     private boolean navigateOnActiveFeedView() {
@@ -122,7 +178,7 @@ public class NavigationActionFactory implements IExecutableExtensionFactory, IEx
   }
 
   /** Enumeration with all possible types of NavigationAction */
-  public enum Actions {
+  enum Actions {
 
     /** Action: Go to the next News */
     NEXT_NEWS("nextNews", true, true, false),
@@ -146,7 +202,13 @@ public class NavigationActionFactory implements IExecutableExtensionFactory, IEx
     PREVIOUS_FEED("previousFeed", false, false, false),
 
     /** Action: Go to the previous unread Feed */
-    PREVIOUS_UNREAD_FEED("previousUnreadFeed", false, false, true);
+    PREVIOUS_UNREAD_FEED("previousUnreadFeed", false, false, true),
+
+    /** Action: Go to next Tab */
+    NEXT_TAB("nextTab", false, false, false),
+
+    /** Action: Go to previous Tab */
+    PREVIOUS_TAB("previousTab", false, false, false);
 
     String fId;
     boolean fIsNewsScoped;
@@ -207,6 +269,12 @@ public class NavigationActionFactory implements IExecutableExtensionFactory, IEx
 
     if (Actions.PREVIOUS_UNREAD_FEED.getId().equals(fId))
       return new NavigationAction(Actions.PREVIOUS_UNREAD_FEED);
+
+    if (Actions.NEXT_TAB.getId().equals(fId))
+      return new NavigationAction(Actions.NEXT_TAB);
+
+    if (Actions.PREVIOUS_TAB.getId().equals(fId))
+      return new NavigationAction(Actions.PREVIOUS_TAB);
 
     return null;
   }
