fdf84ee454ab5e32711d561fecc317c4c9eb325e
bpasero <bpasero@f6fc0b4b-8082-4f21-9ead-586da66f3ff6>
2009-06-18 14:40:39 +0000

    Polish

--- trunk/org.rssowl.ui/src/org/rssowl/ui/internal/editors/feed/NewsBrowserLabelProvider.java
+++ trunk/org.rssowl.ui/src/org/rssowl/ui/internal/editors/feed/NewsBrowserLabelProvider.java
@@ -271,7 +271,7 @@ public class NewsBrowserLabelProvider extends LabelProvider {
     writer.append("div.subline { margin: 0; padding: 0; clear: left; ").append(fSmallFontCSS).append(" }\n");
     writer.append("table.subline { margin: 0; padding: 0; }\n");
     writer.append("tr.subline { margin: 0; padding: 0; }\n");
-    writer.append("td.subline { margin: 0; padding: 0; color: rgb(80, 80, 80); padding-right: 5px; ").append(fSmallFontCSS).append(" }\n");
+    writer.append("td.subline { margin: 0; padding: 0; color: rgb(80, 80, 80); padding-right: 8px; ").append(fSmallFontCSS).append(" }\n");
 
     /* Date */
     writer.append("div.date { float: left; ").append(fSmallFontCSS).append(" }\n");
