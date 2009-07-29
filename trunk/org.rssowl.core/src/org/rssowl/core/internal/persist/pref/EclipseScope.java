6591b24a93121b6d31e91ecd372c04877abfc7a3
bpasero <bpasero@f6fc0b4b-8082-4f21-9ead-586da66f3ff6>
2009-07-29 12:07:23 +0000

    Fix saving of preferences to EclipseCope

--- trunk/org.rssowl.core/src/org/rssowl/core/internal/persist/pref/EclipseScope.java
+++ trunk/org.rssowl.core/src/org/rssowl/core/internal/persist/pref/EclipseScope.java
@@ -91,62 +91,70 @@ public class EclipseScope implements IPreferenceScope {
   }
 
   /*
-   * @see org.rssowl.core.persist.pref.IPreferenceScope#getBoolean(java.lang.String)
+   * @see
+   * org.rssowl.core.persist.pref.IPreferenceScope#getBoolean(java.lang.String)
    */
   public boolean getBoolean(String key) {
     return fPrefService.getBoolean(ROOT_NAME, key, fParent.getBoolean(key), null);
   }
 
   /*
-   * @see org.rssowl.core.persist.pref.IPreferenceScope#getInteger(java.lang.String)
+   * @see
+   * org.rssowl.core.persist.pref.IPreferenceScope#getInteger(java.lang.String)
    */
   public int getInteger(String key) {
     return fPrefService.getInt(ROOT_NAME, key, fParent.getInteger(key), null);
   }
 
   /*
-   * @see org.rssowl.core.persist.pref.IPreferenceScope#getIntegers(java.lang.String)
+   * @see
+   * org.rssowl.core.persist.pref.IPreferenceScope#getIntegers(java.lang.String)
    */
   public int[] getIntegers(String key) {
     throw new UnsupportedOperationException();
   }
 
   /*
-   * @see org.rssowl.core.persist.pref.IPreferenceScope#getLong(java.lang.String)
+   * @see
+   * org.rssowl.core.persist.pref.IPreferenceScope#getLong(java.lang.String)
    */
   public long getLong(String key) {
     return fPrefService.getLong(ROOT_NAME, key, fParent.getLong(key), null);
   }
 
   /*
-   * @see org.rssowl.core.persist.pref.IPreferenceScope#getLongs(java.lang.String)
+   * @see
+   * org.rssowl.core.persist.pref.IPreferenceScope#getLongs(java.lang.String)
    */
   public long[] getLongs(String key) {
     throw new UnsupportedOperationException();
   }
 
   /*
-   * @see org.rssowl.core.persist.pref.IPreferenceScope#getString(java.lang.String)
+   * @see
+   * org.rssowl.core.persist.pref.IPreferenceScope#getString(java.lang.String)
    */
   public String getString(String key) {
     return fPrefService.getString(ROOT_NAME, key, fParent.getString(key), null);
   }
 
   /*
-   * @see org.rssowl.core.persist.pref.IPreferenceScope#getStrings(java.lang.String)
+   * @see
+   * org.rssowl.core.persist.pref.IPreferenceScope#getStrings(java.lang.String)
    */
   public String[] getStrings(String key) {
     throw new UnsupportedOperationException();
   }
 
   /*
-   * @see org.rssowl.core.persist.pref.IPreferenceScope#putBoolean(java.lang.String,
+   * @see
+   * org.rssowl.core.persist.pref.IPreferenceScope#putBoolean(java.lang.String,
    * boolean)
    */
   public void putBoolean(String key, boolean value) {
     Assert.isTrue(key.contains(NODE_SEPARATOR), "Invalid Eclipse Preferences Key!");
 
-    String nodePath = ROOT_NAME + key.substring(0, key.lastIndexOf(NODE_SEPARATOR));
+    String nodePath = getNodePath(key);
     key = key.substring(key.lastIndexOf(NODE_SEPARATOR) + 1);
 
     Preferences prefNode = fRootNode.node(nodePath);
@@ -154,13 +162,14 @@ public class EclipseScope implements IPreferenceScope {
   }
 
   /*
-   * @see org.rssowl.core.persist.pref.IPreferenceScope#putInteger(java.lang.String,
+   * @see
+   * org.rssowl.core.persist.pref.IPreferenceScope#putInteger(java.lang.String,
    * int)
    */
   public void putInteger(String key, int value) {
     Assert.isTrue(key.contains(NODE_SEPARATOR), "Invalid Eclipse Preferences Key!");
 
-    String nodePath = ROOT_NAME + key.substring(0, key.lastIndexOf(NODE_SEPARATOR));
+    String nodePath = getNodePath(key);
     key = key.substring(key.lastIndexOf(NODE_SEPARATOR) + 1);
 
     Preferences prefNode = fRootNode.node(nodePath);
@@ -168,7 +177,8 @@ public class EclipseScope implements IPreferenceScope {
   }
 
   /*
-   * @see org.rssowl.core.persist.pref.IPreferenceScope#putIntegers(java.lang.String,
+   * @see
+   * org.rssowl.core.persist.pref.IPreferenceScope#putIntegers(java.lang.String,
    * int[])
    */
   public void putIntegers(String key, int[] values) {
@@ -176,13 +186,14 @@ public class EclipseScope implements IPreferenceScope {
   }
 
   /*
-   * @see org.rssowl.core.persist.pref.IPreferenceScope#putLong(java.lang.String,
+   * @see
+   * org.rssowl.core.persist.pref.IPreferenceScope#putLong(java.lang.String,
    * long)
    */
   public void putLong(String key, long value) {
     Assert.isTrue(key.contains(NODE_SEPARATOR), "Invalid Eclipse Preferences Key!");
 
-    String nodePath = ROOT_NAME + key.substring(0, key.lastIndexOf(NODE_SEPARATOR));
+    String nodePath = getNodePath(key);
     key = key.substring(key.lastIndexOf(NODE_SEPARATOR) + 1);
 
     Preferences prefNode = fRootNode.node(nodePath);
@@ -190,7 +201,8 @@ public class EclipseScope implements IPreferenceScope {
   }
 
   /*
-   * @see org.rssowl.core.persist.pref.IPreferenceScope#putLongs(java.lang.String,
+   * @see
+   * org.rssowl.core.persist.pref.IPreferenceScope#putLongs(java.lang.String,
    * long[])
    */
   public void putLongs(String key, long[] values) {
@@ -198,13 +210,14 @@ public class EclipseScope implements IPreferenceScope {
   }
 
   /*
-   * @see org.rssowl.core.persist.pref.IPreferenceScope#putString(java.lang.String,
+   * @see
+   * org.rssowl.core.persist.pref.IPreferenceScope#putString(java.lang.String,
    * java.lang.String)
    */
   public void putString(String key, String value) {
     Assert.isTrue(key.contains(NODE_SEPARATOR), "Invalid Eclipse Preferences Key!");
 
-    String nodePath = ROOT_NAME + key.substring(0, key.lastIndexOf(NODE_SEPARATOR));
+    String nodePath = getNodePath(key);
     key = key.substring(key.lastIndexOf(NODE_SEPARATOR) + 1);
 
     Preferences prefNode = fRootNode.node(nodePath);
@@ -212,10 +225,18 @@ public class EclipseScope implements IPreferenceScope {
   }
 
   /*
-   * @see org.rssowl.core.persist.pref.IPreferenceScope#putStrings(java.lang.String,
+   * @see
+   * org.rssowl.core.persist.pref.IPreferenceScope#putStrings(java.lang.String,
    * java.lang.String[])
    */
   public void putStrings(String key, String[] values) {
     throw new UnsupportedOperationException();
+  }
+
+  private String getNodePath(String key) {
+    if (key.startsWith("/"))
+      return key.substring(0, key.lastIndexOf(NODE_SEPARATOR));
+
+    return ROOT_NAME + key.substring(0, key.lastIndexOf(NODE_SEPARATOR));
   }
 }
