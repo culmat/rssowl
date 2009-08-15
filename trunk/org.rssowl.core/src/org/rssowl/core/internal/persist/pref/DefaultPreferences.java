fa31db9f7bbab9ea01f33ccb1dfb521ea265ef17
bpasero <bpasero@f6fc0b4b-8082-4f21-9ead-586da66f3ff6>
2009-08-15 17:31:17 +0000

    Save and Restore up to 5 Import Sources

--- trunk/org.rssowl.core/src/org/rssowl/core/internal/persist/pref/DefaultPreferences.java
+++ trunk/org.rssowl.core/src/org/rssowl/core/internal/persist/pref/DefaultPreferences.java
@@ -295,6 +295,9 @@ public class DefaultPreferences {
   /** Global: Hide Completed Downloads */
   public static final String HIDE_COMPLETED_DOWNLOADS = Preferences.HIDE_COMPLETED_DOWNLOADS.id();
 
+  /** Global: List of Import Sources */
+  public static final String IMPORT_SOURCES = Preferences.IMPORT_SOURCES.id();
+
   /**
    * Eclipse Preferences Follow
    */
