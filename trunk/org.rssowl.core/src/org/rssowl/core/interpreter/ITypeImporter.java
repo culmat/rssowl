aa8c29ed4b243a38abd406d2244da00d5d3086c3
bpasero <bpasero@f6fc0b4b-8082-4f21-9ead-586da66f3ff6>
2009-09-04 18:46:39 +0000

    Bug 817: Externalize Strings

--- trunk/org.rssowl.core/src/org/rssowl/core/interpreter/ITypeImporter.java
+++ trunk/org.rssowl.core/src/org/rssowl/core/interpreter/ITypeImporter.java
@@ -39,22 +39,22 @@ import java.util.List;
 public interface ITypeImporter {
 
   /** Key to store the actual ID of an {@link IEntity} if required */
-  public static final String ID_KEY = "org.rssowl.core.interpreter.typeimporter.EntityId";
+  public static final String ID_KEY = "org.rssowl.core.interpreter.typeimporter.EntityId"; //$NON-NLS-1$
 
   /** Key to store extra data with the {@link IEntity} if required */
-  public static final String DATA_KEY = "org.rssowl.core.interpreter.typeimporter.DataId";
+  public static final String DATA_KEY = "org.rssowl.core.interpreter.typeimporter.DataId"; //$NON-NLS-1$
 
   /**
    * Key to store a boolean indicating that a Folder was only created as
    * temporary container
    */
-  public static final String TEMPORARY_FOLDER = "org.rssowl.core.interpreter.typeimporter.TemporaryFolder";
+  public static final String TEMPORARY_FOLDER = "org.rssowl.core.interpreter.typeimporter.TemporaryFolder"; //$NON-NLS-1$
 
   /** Key to store the Homepage with the {@link IEntity} */
-  public static final String HOMEPAGE_KEY = "org.rssowl.core.interpreter.typeimporter.Homepage";
+  public static final String HOMEPAGE_KEY = "org.rssowl.core.interpreter.typeimporter.Homepage"; //$NON-NLS-1$
 
   /** Key to store the Description with the {@link IEntity} */
-  public static final String DESCRIPTION_KEY = "org.rssowl.core.interpreter.typeimporter.Description";
+  public static final String DESCRIPTION_KEY = "org.rssowl.core.interpreter.typeimporter.Description"; //$NON-NLS-1$
 
   /**
    * Import a Type from the given Document. A very common usecase is importing
