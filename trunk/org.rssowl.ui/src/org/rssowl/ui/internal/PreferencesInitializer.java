a3b42567f85794cece52ef9324909448c5c2dbca
bpasero <bpasero@f6fc0b4b-8082-4f21-9ead-586da66f3ff6>
2009-09-09 21:33:53 +0000

    Change defaults: Dont delete unread news via Retention

--- trunk/org.rssowl.ui/src/org/rssowl/ui/internal/PreferencesInitializer.java
+++ trunk/org.rssowl.ui/src/org/rssowl/ui/internal/PreferencesInitializer.java
@@ -140,6 +140,7 @@ public class PreferencesInitializer implements IPreferencesInitializer {
     defaultScope.putBoolean(Preference.DEL_NEWS_BY_COUNT_STATE.id(), true);
     defaultScope.putInteger(Preference.DEL_NEWS_BY_COUNT_VALUE.id(), 200);
     defaultScope.putInteger(Preference.DEL_NEWS_BY_AGE_VALUE.id(), 30);
+    defaultScope.putBoolean(Preference.NEVER_DEL_UNREAD_NEWS_STATE.id(), true);
   }
 
   /**
