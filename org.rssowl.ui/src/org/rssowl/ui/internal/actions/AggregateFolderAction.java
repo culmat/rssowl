274d1f6d5be81b28bcdc0eac29095de8af038655
bpasero <bpasero@f6fc0b4b-8082-4f21-9ead-586da66f3ff6>
2009-09-14 17:52:02 +0000

    Improve guidance for "Aggregate News"

--- org.rssowl.ui/src/org/rssowl/ui/internal/actions/AggregateFolderAction.java
+++ org.rssowl.ui/src/org/rssowl/ui/internal/actions/AggregateFolderAction.java
@@ -25,6 +25,7 @@
 package org.rssowl.ui.internal.actions;
 
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredSelection;
@@ -32,13 +33,36 @@ import org.eclipse.swt.custom.BusyIndicator;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.PlatformUI;
+import org.rssowl.core.Owl;
+import org.rssowl.core.internal.persist.pref.DefaultPreferences;
+import org.rssowl.core.persist.IBookMark;
 import org.rssowl.core.persist.IFolder;
+import org.rssowl.core.persist.IFolderChild;
+import org.rssowl.core.persist.IMark;
+import org.rssowl.core.persist.IModelFactory;
+import org.rssowl.core.persist.INews;
+import org.rssowl.core.persist.ISearchCondition;
+import org.rssowl.core.persist.ISearchField;
+import org.rssowl.core.persist.ISearchMark;
+import org.rssowl.core.persist.SearchSpecifier;
+import org.rssowl.core.persist.dao.DynamicDAO;
+import org.rssowl.core.persist.pref.IPreferenceScope;
+import org.rssowl.core.util.CoreUtils;
 import org.rssowl.ui.internal.FolderNewsMark;
 import org.rssowl.ui.internal.OwlUI;
+import org.rssowl.ui.internal.dialogs.AggregateNewsDialog;
+import org.rssowl.ui.internal.dialogs.SearchMarkDialog;
+import org.rssowl.ui.internal.util.ModelUtils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Aggregates all bookmarks of a selected folder to be displayed in the
- * feedview.
+ * feedview. Asks the user to either use a saved search or
+ * {@link FolderNewsMark} to display.
  *
  * @author bpasero
  */
@@ -61,21 +85,127 @@ public class AggregateFolderAction implements IObjectActionDelegate {
    */
   public void run(IAction action) {
     if (!fSelection.isEmpty() && fSelection instanceof IStructuredSelection) {
-      final Object firstElem = ((IStructuredSelection) fSelection).getFirstElement();
+      Object firstElem = ((IStructuredSelection) fSelection).getFirstElement();
+
+      /* Aggregate News of Folder */
       if (firstElem instanceof IFolder) {
+        IPreferenceScope preferences = Owl.getPreferenceService().getGlobalScope();
+        boolean aggregateAsSearch = preferences.getBoolean(DefaultPreferences.AGGREGATE_NEWS_AS_SEARCH);
+        boolean askUserForAggregationOption = !preferences.getBoolean(DefaultPreferences.REMEMBER_AGGREGATE_NEWS_OPTION);
 
-        /* Create in-memory Newsmark */
-        BusyIndicator.showWhile(PlatformUI.getWorkbench().getDisplay(), new Runnable() {
-          public void run() {
-            FolderNewsMark folderNewsMark = new FolderNewsMark((IFolder) firstElem);
-            StructuredSelection newSelection = new StructuredSelection(folderNewsMark);
+        /* Ask user for Aggregation Mode if required */
+        if (askUserForAggregationOption) {
+          AggregateNewsDialog dialog = new AggregateNewsDialog(fTargetPart.getSite().getShell(), ((IFolder) firstElem).getName());
+          int res = dialog.open();
 
-            /* Open in Feedview */
-            OwlUI.openInFeedView(fTargetPart.getSite().getPage(), newSelection);
-          }
-        });
+          /* Check for Cancellation */
+          if (res == IDialogConstants.CANCEL_ID)
+            return;
+
+          aggregateAsSearch = (res == IDialogConstants.YES_ID);
+        }
+
+        /* Create Search on Folder */
+        if (aggregateAsSearch)
+          createAndOpenSearch((IFolder) firstElem);
+
+        /* Otherwise directly aggregate */
+        else
+          aggregateFolder((IFolder) firstElem);
       }
     }
+  }
+
+  private void createAndOpenSearch(IFolder folder) {
+    ISearchMark locationSearch = findSearch(folder);
+    if (locationSearch == null) {
+      IModelFactory factory = Owl.getModelFactory();
+      List<ISearchCondition> conditions = new ArrayList<ISearchCondition>();
+
+      ISearchField locationField = factory.createSearchField(INews.LOCATION, INews.class.getName());
+      conditions.add(factory.createSearchCondition(locationField, SearchSpecifier.IS, ModelUtils.toPrimitive(Collections.singletonList((IFolderChild) folder))));
+
+      SearchMarkDialog dialog = new SearchMarkDialog(fTargetPart.getSite().getShell(), folder.getParent(), folder, conditions, true);
+      if (dialog.open() == IDialogConstants.OK_ID)
+        locationSearch = dialog.getSearchMark();
+      else
+        return;
+    }
+
+    /* Open Search and Reload Bookmarks that have never been reloaded before */
+    if (locationSearch != null) {
+      OwlUI.openInFeedView(fTargetPart.getSite().getPage(), new StructuredSelection(locationSearch));
+
+      List<IBookMark> bookMarksToReload = new ArrayList<IBookMark>();
+      fillBookMarksToReload(bookMarksToReload, folder);
+      if (!bookMarksToReload.isEmpty())
+        new ReloadTypesAction(new StructuredSelection(bookMarksToReload.toArray()), fTargetPart.getSite().getShell()).run();
+    }
+  }
+
+  private void fillBookMarksToReload(List<IBookMark> bookMarksToReload, IFolder folder) {
+    List<IMark> marks = folder.getMarks();
+    for (IMark mark : marks) {
+      if (mark instanceof IBookMark) {
+        if ((((IBookMark) mark).getMostRecentNewsDate() == null))
+          bookMarksToReload.add((IBookMark) mark);
+      }
+    }
+
+    List<IFolder> childs = folder.getFolders();
+    for (IFolder child : childs) {
+      fillBookMarksToReload(bookMarksToReload, child);
+    }
+  }
+
+  private ISearchMark findSearch(IFolderChild folder) {
+    Collection<ISearchMark> existingSearches = DynamicDAO.loadAll(ISearchMark.class);
+    for (ISearchMark search : existingSearches) {
+      List<ISearchCondition> conditions = search.getSearchConditions();
+      if (conditions.size() == 1) {
+        ISearchCondition condition = conditions.get(0);
+
+        /* Check on Search Field */
+        ISearchField field = condition.getField();
+        if (field.getId() != INews.LOCATION)
+          continue;
+
+        /* Check on Search Specifier */
+        SearchSpecifier specifier = condition.getSpecifier();
+        if (specifier != SearchSpecifier.IS)
+          continue;
+
+        /* Check on Search Value */
+        Object value = condition.getValue();
+        if (!(value instanceof Long[][]))
+          continue;
+
+        Long[][] valueLong = (Long[][]) value;
+        if (valueLong.length == 0)
+          continue;
+
+        if (valueLong[CoreUtils.FOLDER].length != 1)
+          continue;
+
+        if (valueLong[CoreUtils.FOLDER][0] == folder.getId())
+          return search;
+      }
+    }
+
+    return null;
+  }
+
+  /* Create in-memory Newsmark */
+  private void aggregateFolder(final IFolder folder) {
+    BusyIndicator.showWhile(PlatformUI.getWorkbench().getDisplay(), new Runnable() {
+      public void run() {
+        FolderNewsMark folderNewsMark = new FolderNewsMark(folder);
+        StructuredSelection newSelection = new StructuredSelection(folderNewsMark);
+
+        /* Open in Feedview */
+        OwlUI.openInFeedView(fTargetPart.getSite().getPage(), newSelection);
+      }
+    });
   }
 
   /*
