57762edb2abc814cbbdcf100365fc25829b32b8c
bpasero <bpasero@f6fc0b4b-8082-4f21-9ead-586da66f3ff6>
2009-09-21 17:03:25 +0000

    Implement Eclipse Wizards

--- trunk/org.rssowl.ui/src/org/rssowl/ui/internal/dialogs/NewsBinWizard.java
+++ trunk/org.rssowl.ui/src/org/rssowl/ui/internal/dialogs/NewsBinWizard.java
@@ -28,11 +28,29 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
+import org.rssowl.core.Owl;
+import org.rssowl.core.persist.IFolder;
+import org.rssowl.core.persist.IFolderChild;
+import org.rssowl.core.persist.IModelFactory;
+import org.rssowl.core.persist.INewsBin;
+import org.rssowl.core.persist.dao.DynamicDAO;
+import org.rssowl.core.util.Pair;
+import org.rssowl.core.util.StringUtils;
 import org.rssowl.ui.internal.OwlUI;
+import org.rssowl.ui.internal.util.FolderChooser;
+import org.rssowl.ui.internal.util.LayoutUtils;
+import org.rssowl.ui.internal.util.FolderChooser.ExpandStrategy;
+
+import java.io.Serializable;
+import java.util.Map;
 
 /**
  * The {@link NewsBinWizard} is only used in the Eclipse Integration to create
@@ -41,9 +59,14 @@ import org.rssowl.ui.internal.OwlUI;
  * @author bpasero
  */
 public class NewsBinWizard extends Wizard implements INewWizard {
+  private IFolder fFolder;
+  private IFolderChild fPosition;
+  private NewNewsBinWizardPage fPage;
 
   /* Page for Wizard */
   private class NewNewsBinWizardPage extends WizardPage {
+    private Text fNameInput;
+    private FolderChooser fFolderChooser;
 
     NewNewsBinWizardPage(String pageName) {
       super(pageName, pageName, OwlUI.getImageDescriptor("icons/wizban/newsbin_wiz.gif")); //$NON-NLS-1$
@@ -52,13 +75,52 @@ public class NewsBinWizard extends Wizard implements INewWizard {
 
     public void createControl(Composite parent) {
       Composite control = new Composite(parent, SWT.NONE);
-      control.setLayout(new GridLayout(2, false));
+      control.setLayout(LayoutUtils.createGridLayout(2, 5, 5));
+      control.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false));
+
+      Label l1 = new Label(control, SWT.NONE);
+      l1.setText(Messages.NewsBinWizard_NAME);
+
+      Composite nameContainer = new Composite(control, SWT.BORDER);
+      nameContainer.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false));
+      nameContainer.setLayout(LayoutUtils.createGridLayout(2, 0, 0));
+      nameContainer.setBackground(control.getDisplay().getSystemColor(SWT.COLOR_LIST_BACKGROUND));
+
+      fNameInput = new Text(nameContainer, SWT.SINGLE);
+      fNameInput.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, true));
+      fNameInput.addModifyListener(new ModifyListener() {
+        public void modifyText(ModifyEvent e) {
+          setErrorMessage(null);
+        }
+      });
+
+      Label l3 = new Label(control, SWT.NONE);
+      l3.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
+      l3.setText(Messages.NewsBinWizard_LOCATION);
+
+      /* Folder Chooser */
+      fFolderChooser = new FolderChooser(control, fFolder, SWT.BORDER, true);
+      fFolderChooser.setExpandStrategy(ExpandStrategy.PACK);
+      fFolderChooser.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false));
+      fFolderChooser.setLayout(LayoutUtils.createGridLayout(1, 0, 0, 2, 5, false));
+      fFolderChooser.setBackground(control.getDisplay().getSystemColor(SWT.COLOR_LIST_BACKGROUND));
 
       setControl(control);
     }
-  }
 
-  private NewNewsBinWizardPage fPage;
+    String getBinName() {
+      return fNameInput.getText();
+    }
+
+    void focusInput() {
+      fNameInput.setFocus();
+      fNameInput.selectAll();
+    }
+
+    IFolder getFolder() {
+      return fFolderChooser.getFolder();
+    }
+  }
 
   /** Leave for Reflection */
   public NewsBinWizard() {}
@@ -66,7 +128,11 @@ public class NewsBinWizard extends Wizard implements INewWizard {
   /*
    * @see org.eclipse.ui.IWorkbenchWizard#init(org.eclipse.ui.IWorkbench, org.eclipse.jface.viewers.IStructuredSelection)
    */
-  public void init(IWorkbench workbench, IStructuredSelection selection) {}
+  public void init(IWorkbench workbench, IStructuredSelection selection) {
+    Pair<IFolder, IFolderChild> pair = OwlUI.getLocationAndPosition(selection);
+    fFolder = pair.getFirst();
+    fPosition = pair.getSecond();
+  }
 
   /*
    * @see org.eclipse.jface.wizard.Wizard#addPages()
@@ -84,6 +150,30 @@ public class NewsBinWizard extends Wizard implements INewWizard {
    */
   @Override
   public boolean performFinish() {
-    return false;
+    String name = fPage.getBinName();
+    IModelFactory factory = Owl.getModelFactory();
+
+    /* Require Name */
+    if (!StringUtils.isSet(name)) {
+      fPage.setErrorMessage(Messages.NewsBinWizard_ENTER_NAME);
+      fPage.focusInput();
+      return false;
+    }
+
+    /* Get the parent Folder */
+    IFolder parent = fPage.getFolder();
+
+    /* Create the NewsBin */
+    INewsBin fNewsbin = factory.createNewsBin(null, parent, name, fPosition, fPosition != null ? true : null);
+
+    /* Copy all Properties from Parent into this Mark */
+    Map<String, Serializable> properties = parent.getProperties();
+
+    for (Map.Entry<String, Serializable> property : properties.entrySet())
+      fNewsbin.setProperty(property.getKey(), property.getValue());
+
+    DynamicDAO.save(parent);
+
+    return true;
   }
 }
