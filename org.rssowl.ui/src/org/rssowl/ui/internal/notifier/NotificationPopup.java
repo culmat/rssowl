1306ee2380f7c0d77d127f677e5fefe29c7e6cde
bpasero <bpasero@f6fc0b4b-8082-4f21-9ead-586da66f3ff6>
2010-01-22 17:47:18 +0000

    Close Notifier when deactivated

--- org.rssowl.ui/src/org/rssowl/ui/internal/notifier/NotificationPopup.java
+++ org.rssowl.ui/src/org/rssowl/ui/internal/notifier/NotificationPopup.java
@@ -58,7 +58,9 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Monitor;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchPage;
@@ -902,6 +904,14 @@ public class NotificationPopup extends PopupDialog {
           onNavNext();
         else if (e.count > 0)
           onNavPrevious();
+      }
+    });
+
+    /* Close Shell once deactivated and not incoming automatic */
+    fShell.addListener(SWT.Deactivate, new Listener() {
+      public void handleEvent(Event event) {
+        if (fMode != Mode.INCOMING_AUTOMATIC && fGlobalScope.getBoolean(DefaultPreferences.CLOSE_NOTIFIER_ON_OPEN))
+          doClose();
       }
     });
 
