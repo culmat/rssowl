0b68c11155ac50828be594fa6349233157985482
bpasero <bpasero@f6fc0b4b-8082-4f21-9ead-586da66f3ff6>
2010-02-23 00:14:40 +0000

    Better separate between filterbar and browser on Mac

--- org.rssowl.ui/src/org/rssowl/ui/internal/editors/feed/FeedView.java
+++ org.rssowl.ui/src/org/rssowl/ui/internal/editors/feed/FeedView.java
@@ -1798,7 +1798,7 @@ public class FeedView extends EditorPart implements IReusableEditor {
     fFilterBar = new FilterBar(this, rootComposite);
 
     /* Separate from SashForm */
-    if (!Application.IS_MAC) {
+    if (!Application.IS_MAC || fInitialBrowserMaximized) {
       Label sep = new Label(rootComposite, SWT.SEPARATOR | SWT.HORIZONTAL);
       sep.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false));
     }
