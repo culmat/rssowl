4014a6c60f10cf4446f2b65306859697b67295a1
bpasero <bpasero@f6fc0b4b-8082-4f21-9ead-586da66f3ff6>
2011-05-27 08:45:21 +0000

    Try to run more tests on nightly engine

--- trunk/org.rssowl.core.tests/src/org/rssowl/core/tests/LocalTests.java
+++ trunk/org.rssowl.core.tests/src/org/rssowl/core/tests/LocalTests.java
@@ -30,22 +30,33 @@ import org.junit.runners.Suite.SuiteClasses;
 import org.rssowl.core.tests.controller.ControllerTestLocal;
 import org.rssowl.core.tests.controller.ControllerTestLocalLBS;
 import org.rssowl.core.tests.controller.ReloadTestLocal;
+import org.rssowl.core.tests.controller.ReloadTestLocalLBS;
 import org.rssowl.core.tests.importer.ImporterTest;
 import org.rssowl.core.tests.interpreter.InterpreterTest;
 import org.rssowl.core.tests.model.ApplicationLayerTest;
+import org.rssowl.core.tests.model.ApplicationLayerTestLBS;
 import org.rssowl.core.tests.model.CachingDAOTest;
+import org.rssowl.core.tests.model.CachingDAOTestLBS;
 import org.rssowl.core.tests.model.DBManagerTest;
+import org.rssowl.core.tests.model.DBManagerTestLBS;
 import org.rssowl.core.tests.model.ModelSearchTest1;
 import org.rssowl.core.tests.model.ModelSearchTest2;
 import org.rssowl.core.tests.model.ModelSearchTest3;
 import org.rssowl.core.tests.model.ModelSearchTest4;
 import org.rssowl.core.tests.model.ModelTest1;
+import org.rssowl.core.tests.model.ModelTest1LBS;
 import org.rssowl.core.tests.model.ModelTest2;
+import org.rssowl.core.tests.model.ModelTest2LBS;
 import org.rssowl.core.tests.model.ModelTest3;
+import org.rssowl.core.tests.model.ModelTest3LBS;
 import org.rssowl.core.tests.model.ModelTest4;
+import org.rssowl.core.tests.model.ModelTest4LBS;
 import org.rssowl.core.tests.model.NewsFilterTest;
+import org.rssowl.core.tests.model.NewsFilterTestLBS;
 import org.rssowl.core.tests.model.PreferencesDAOTest;
+import org.rssowl.core.tests.model.PreferencesDAOTestLBS;
 import org.rssowl.core.tests.model.PreferencesScopeTest;
+import org.rssowl.core.tests.model.PreferencesScopeTestLBS;
 import org.rssowl.core.tests.persist.INewsTest;
 import org.rssowl.core.tests.persist.LongArrayListTest;
 import org.rssowl.core.tests.persist.MigrationsTest;
@@ -71,31 +82,31 @@ import org.rssowl.core.tests.util.URIUtilsTest;
   ControllerTestLocal.class,
   ControllerTestLocalLBS.class, //Running twice to test with large block size
   ReloadTestLocal.class,
-  ReloadTestLocal.class, //Running twice to test with large block size
+  ReloadTestLocalLBS.class, //Running twice to test with large block size
   ModelTest1.class,
-  ModelTest1.class, //Running twice to test with large block size
+  ModelTest1LBS.class, //Running twice to test with large block size
   ModelTest2.class,
-  ModelTest2.class, //Running twice to test with large block size
+  ModelTest2LBS.class, //Running twice to test with large block size
   ModelTest3.class,
-  ModelTest3.class, //Running twice to test with large block size
+  ModelTest3LBS.class, //Running twice to test with large block size
   ModelTest4.class,
-  ModelTest4.class, //Running twice to test with large block size
+  ModelTest4LBS.class, //Running twice to test with large block size
   PreferencesDAOTest.class,
-  PreferencesDAOTest.class, //Running twice to test with large block size
+  PreferencesDAOTestLBS.class, //Running twice to test with large block size
   ApplicationLayerTest.class,
-  ApplicationLayerTest.class, //Running twice to test with large block size
+  ApplicationLayerTestLBS.class, //Running twice to test with large block size
   NewsFilterTest.class,
-  NewsFilterTest.class, //Running twice to test with large block size
+  NewsFilterTestLBS.class, //Running twice to test with large block size
   ModelSearchTest1.class,
   ModelSearchTest2.class,
   ModelSearchTest3.class,
   ModelSearchTest4.class,
   DBManagerTest.class,
-  DBManagerTest.class, //Running twice to test with large block size
+  DBManagerTestLBS.class, //Running twice to test with large block size
   CachingDAOTest.class,
-  CachingDAOTest.class, //Running twice to test with large block size
+  CachingDAOTestLBS.class, //Running twice to test with large block size
   PreferencesScopeTest.class,
-  PreferencesScopeTest.class, //Running twice to test with large block size
+  PreferencesScopeTestLBS.class, //Running twice to test with large block size
   MergeUtilsTest.class,
   INewsTest.class,
   StringUtilsTest.class,
