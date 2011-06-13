1ad7bf0978ce9910842bfbfd24f45d967aa5f9ec
bpasero <bpasero@f6fc0b4b-8082-4f21-9ead-586da66f3ff6>
2011-06-13 08:17:55 +0000

    Link to proper FAQ entry on browser issues

--- org.rssowl.ui/src/org/rssowl/ui/internal/util/CBrowser.java
+++ org.rssowl.ui/src/org/rssowl/ui/internal/util/CBrowser.java
@@ -146,7 +146,7 @@ public class CBrowser {
       box.setText(Messages.CBrowser_ERROR_CREATE_BROWSER);
       box.setMessage(Messages.CBrowser_ERROR_CREATE_BROWSER_MSG);
       if (box.open() == SWT.OK)
-        BrowserUtils.openLinkExternal("http://www.rssowl.org/help"); //$NON-NLS-1$
+        BrowserUtils.openLinkExternal("http://www.rssowl.org/help#item_6j"); //$NON-NLS-1$
 
       throw e;
     }
